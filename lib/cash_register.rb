c8aec7b2fe575991c8b4e574ef9ffc289cebaabd
Amy Fogelman <abfnj1@gmail.com>
2019-07-18 19:30:10 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,9 +8,9 @@ class CashRegister
   end 
   
   def add_item(title,price, quantity = 1)
-    @total += (price + quantity)
+    self.total += (price + quantity)
     quantity.times do
-    @items << title
+    self.items << title
   end
  @last_transaction = (price * quantity)
 end
